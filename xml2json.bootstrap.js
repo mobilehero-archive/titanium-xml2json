e474dc90c88aa04f665b849bcbf122da882d45e3
Brenton House <brenton.house@gmail.com>
2019-07-09 14:25:17 -0500

    added bootstrap

---
+++ xml2json.bootstrap.js
@@ -0,0 +1,3 @@
+require('/ti.internal/extensions/binding')('xml2json', '/node_modules/@titanium/xml2json');
+require('/ti.internal/extensions/binding')('xmltojson', '/node_modules/@titanium/xml2json');
+
