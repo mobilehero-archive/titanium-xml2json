157020786f97aa1467a25fcbed3607075c15d3db
Brenton House <brenton.house@gmail.com>
2019-08-14 15:13:42 -0500

    updates for 8.1.0.GA SDK

--- xml2json.bootstrap.js
+++ xml2json.bootstrap.js
@@ -1,3 +1,9 @@
-require('/ti.internal/extensions/binding')('xml2json', '/node_modules/@titanium/xml2json');
-require('/ti.internal/extensions/binding')('xmltojson', '/node_modules/@titanium/xml2json');
+// code for SDK < 8.1.0.GA
+// require('/ti.internal/extensions/binding')('xml2json', '/node_modules/@titanium/xml2json');
+// require('/ti.internal/extensions/binding')('xmltojson', '/node_modules/@titanium/xml2json');
+
+// Code for SDK >= 8.1.0.GA
+global.binding.register('xml2json', require('@titanium/xml2json'));
+global.binding.register('xmltojson', require('@titanium/xml2json'));
+
 
